50dacae2a8c790bb33ffb2e02e81ba72f7598333
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-01 15:37:32 +0100

    added a new file

---
+++ src/main/kotlin/collectinginput.kt
@@ -0,0 +1,9 @@
+fun main(){
+    print("Enter text: ")
+    var text = readLine()!!
+    println("You entered: $text")
+
+    print("Enter your favorite number: ")
+    var input1 = readLine()!!
+    println("My favorite number is: $input1")
+}
