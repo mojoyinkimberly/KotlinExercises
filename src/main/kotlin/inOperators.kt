e025b46b9edc47e2fde154f073c51b77d496c54a
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-01 15:58:01 +0100

    added a new file

---
+++ src/main/kotlin/inOperators.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+    val numbers = intArrayOf(1,2,3,4,5)
+    if(5 in numbers) {
+        println("numbers array contains 5")
+    }else{
+        println("numbers array does not contain 5.")
+    }
+}
