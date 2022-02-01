1ab640fad0626addcb88ec75c3a6d39fd2a92324
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-01 16:08:14 +0100

    added a new file

---
+++ src/main/kotlin/whileLoop.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    var num = 10
+    var total = 1
+    while(num >= 5){
+
+        if(num%2 == 0){
+            total = num * 2
+            println("$total")
+        } else {println(num)}
+        num--
+        }
+        }
+
