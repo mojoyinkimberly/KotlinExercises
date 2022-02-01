93e1ab5b89c3500ba1fa323979c04bbf82eb28c7
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-01 15:45:27 +0100

    added a new file

---
+++ src/main/kotlin/comparison.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    val a = -12
+    val b = 12
+    val max = if(a > b) {
+        println("a is greater than b")
+        a
+    }else{
+        println("b is greater than a.")
+        b
+    }
+
+    println("max = $max")
+    }
