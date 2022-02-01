9a815de815b9d912de7ce8b37eceeb2af766e3fe
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-01 14:07:45 +0100

    added a new file

---
+++ src/main/kotlin/collectingInputs.kt
@@ -0,0 +1,9 @@
+import  java.util.Scanner
+fun main(args: Array<String>){
+    val scanner = Scanner(System.`in`)
+    print("Enter a number: ")
+    var number: Int = scanner.nextInt()
+    println("you entered: $number")
+
+
+}
