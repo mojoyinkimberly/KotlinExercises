e41d01c1a56f0411c2e162dd3044d403b5e3f416
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-07 01:19:34 +0100

    added a new file

---
+++ src/main/kotlin/collectingInteger.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    val input = Scanner(System.`in`)
+    print("Enter an integer: ")
+    var number = input.nextInt();
+    println("The integer you entered is: $number")
+
+
+}
