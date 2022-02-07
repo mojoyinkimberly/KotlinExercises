847e04774f2a5bb06082abe331fa34b5eef1ad04
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-07 01:19:11 +0100

    added a new file

---
+++ src/main/kotlin/addingIntegers.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    val input = Scanner(System.`in`)
+    print("Enter the first integer: ")
+    var int1 = input.nextInt();
+    print("Enter the second integer: ")
+    var int2 = input.nextInt();
+    var sum = int1 + int2
+    println("The sum of the two integers entered is : $sum")
+
+}
