62befe6910ef5db9752404a979339ac5eebb8a14
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-07 10:56:57 +0100

    added a new file

--- src/main/kotlin/swappingNumbers.kt
+++ src/main/kotlin/swappingNumbers.kt
@@ -1,4 +1,14 @@
-fun main(args : Array<String>){
+import java.util.*
 
+fun main(args : Array<String>){
+val input = Scanner(System.`in`)
+print("Enter the first number: ")
+var firstNumber =  input.nextInt()
+print("Enter the second number: ")
+var secondNumber = input.nextInt()
+var tempValue = firstNumber
+firstNumber = secondNumber
+secondNumber = tempValue
+println("The new first number is $firstNumber and the second number is $secondNumber")
 
 }
