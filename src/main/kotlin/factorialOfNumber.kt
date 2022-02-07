1a3a787e7a558c85b6ec778ee7df1d96439068a8
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-07 10:55:54 +0100

    added a new file

--- src/main/kotlin/factorialOfNumber.kt
+++ src/main/kotlin/factorialOfNumber.kt
@@ -3,7 +3,6 @@ import java.util.*
 fun main(args : Array<String>){
     var factorial : Long = 1
     val input = Scanner(System.`in`)
-
     print("Enter a number to get its factorial: ")
     var number = input.nextInt()
     for(i in 1..number)
