0d77b22b5fe420cd3061f1657b53407e9e73205c
Mojoyinola Oketope <m.oketope@semicolon.africa>
2022-02-07 01:20:11 +0100

    added a new file

---
+++ src/main/kotlin/findingAscii.kt
@@ -0,0 +1,12 @@
+import java.util.*
+fun main(args: Array<String>){
+ val input = Scanner(System.`in`)
+ print("Enter a character: ") ;
+ var character : Char  = input.next().single();
+ var value = character.toInt();
+
+
+ println("The ascii value of $character is: $value")
+
+
+    }
